06afc8054880ccb16004898139c3adad03337367
Riken-sajal <riken@sajaltech.com>
2024-11-21 12:25:02 +0530

    download csv is now completed

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from django.conf.urls import include
-from .views import download_file
+from .views import download_file, list_csvs, download_csv, download_media_file
 
 urlpatterns = [
     path('downloads/<path:file_path>/', download_file, name='download_file'),
+    path('csvs/', list_csvs, name='list_csvs'),
+    path('csvs/download/<int:config_id>/', download_csv, name='download_csv'),
+    path('media/download/<path:file_path>/', download_media_file, name='download_media_file'),
+
     # path('list_files/', list_files, name='list_file'),
     # path('csv/<str:file_name>', csv_file, name='csv_file'),
     # path('csv/', return_csv_links, name='csv_file_list'),
--- app/views.py
+++ app/views.py
@@ -40,3 +40,78 @@ def download_file(request, file_path):
         return HttpResponse("<br>".join(file_links))
     else:
         HttpResponse("Folder not found.", status=404)
+        
+        
+        
+import csv
+from django.http import HttpResponse
+from django.utils.timezone import now
+from .models import VideosData, configuration
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
+from django.http import FileResponse
+
+def generate_csv(configuration,request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = f'attachment; filename="{configuration.website_name}_{now().strftime("%Y%m%d%H%M%S")}.csv"'
+
+    writer = csv.writer(response)
+
+    writer.writerow([
+        'Video Name', 
+        'Title', 
+        'Username', 
+        'Likes', 
+        'Dislikes', 
+        'Video URL', 
+        'Image URL', 
+        'Direct Video Download Link', 
+        'Direct Image Download Link',
+        'Release Date', 
+        'Poster Image URL'
+    ])
+
+    videos = VideosData.objects.filter(configuration=configuration)
+    for video in videos:
+        video_download_link = request.build_absolute_uri(reverse('download_media_file', args=[video.video.name])) if video.video else ''
+        image_download_link = request.build_absolute_uri(reverse('download_media_file', args=[video.image.name])) if video.image else ''
+        writer.writerow([
+            video.Video_name,
+            video.Title,
+            video.Username,
+            video.Likes,
+            video.Disclike,
+            video.Url,
+            video.Poster_Image_url,
+            video_download_link,  
+            image_download_link,  
+            video.Release_Date,
+        ])
+
+    return response
+
+def download_csv(request, config_id):
+    config = get_object_or_404(configuration, id=config_id)
+
+    return generate_csv(config,request)
+
+
+def list_csvs(request):
+    configs = configuration.objects.all()
+    data = [
+        {
+            'config': config,
+            'download_link': reverse('download_csv', args=[config.id])
+        }
+        for config in configs
+    ]
+    return render(request, 'list_csvs.html', {'data': data})
+
+def download_media_file(request, file_path):
+    full_path = os.path.join(settings.MEDIA_ROOT, file_path)
+
+    if not os.path.exists(full_path):
+        return HttpResponse("File not found.", status=404)
+
+    response = FileResponse(open(full_path, 'rb'), as_attachment=True)
+    return response
